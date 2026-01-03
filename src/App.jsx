35a2ac543f66180fd5b5d08e58bc8caf21aa7097
anmolgautam <anmolgautam2428@gmail.com>
2026-01-03 13:42:35 +0530

    setup the sections and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import { Hero } from "@/sections/Hero";
+import { About } from "@/sections/About";
+import { Navbar } from "@/layout/Navbar";
+import {Projects} from "@/sections/Projects"
+import {Experience} from "@/sections/Experience"
+import {Research} from "@/sections/Research"
+import {Contact} from "@/sections/Contact"
+
 
 function App() {
-
   return (
     <>
-      
+      <div className="min-h-screen overflow-x-hidden">
+        <Navbar/>
+        <main>
+          <Hero />
+          <About />
+          <Projects />
+          <Experience />
+          <Research />
+          <Contact />
+        </main>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,3 @@
+export const Hero = () => {
+  return <section>sadass</section>;
+};
